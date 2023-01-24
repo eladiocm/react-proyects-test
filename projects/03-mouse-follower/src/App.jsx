76b399c1cc85b91465812a4d27c8d096e3a9593b
eladiocm <eladio970826@gmail.com>
2023-01-24 01:14:06 -0500

    segue al cursor

---
+++ projects/03-mouse-follower/src/App.jsx
@@ -0,0 +1,49 @@
+import { useState, useEffect } from 'react'
+
+function App () {
+  const [enable, setEnable] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+
+  useEffect(() => {
+    console.log('first')
+
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      setPosition({ x: clientX, y: clientY })
+    }
+    if (enable) {
+      window.addEventListener('pointermove', handleMove)
+    }
+
+    //! cleanup
+    // ? SE EJECUTA:
+    //! -->  cuando el componente se desmonta
+    //! -->  cuando cambian las dependencias, antes de ejecutar el efecto de nuevo
+    return () => {
+      window.removeEventListener('pointermove', handleMove)
+    }
+  }, [enable])
+
+  return (
+    <>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: '#09f',
+        borderRadius: '50%',
+        opacity: '0.8',
+        pointerEvents: 'none',
+        left: -20,
+        top: -20,
+        width: 40,
+        height: 40,
+        transform: `translate(${position.x}px, ${position.y}px)`
+      }}
+      />
+      <button onClick={() => setEnable(!enable)}>
+        {enable ? 'Desacticar' : 'Activar'} seguir puntero
+      </button>
+    </>
+  )
+}
+
+export default App
